ed23d5311ba264d3d657f335ff1f6bbe3c02d369
JhonOliveros0 <jhonoliveros0@gmail.com>
2024-08-25 02:20:44 -0500

    continuacion logica del boton encriptar

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function encriptar() {
     let texto = document.getElementById("texto").value;
     let tituloMensaje = document.getElementById("titulo");
     let parrafo = document.getElementById("parrafo");
-
+    let imagen = document.getElementById("inicio");
     let textoEncriptado = texto
         .replace(/e/gi, "enter")
         .replace(/i/gi, "imes")
@@ -11,10 +11,14 @@ function encriptar() {
         .replace(/u/gi, "ufat");
 
     if (texto.length != 0) {
-        texto = textoEncriptado;
+        document.getElementById("texto").value = textoEncriptado;
         tituloMensaje.textContent = "Texto Encriptado con éxito";
         parrafo.textContent = "";
+        imagen.src = "img/Encriptado.jpg";
     } else {
+        imagen.src = "./img/muñeco.png";
+        tituloMensaje.textContent = "Ningún mensaje encontrado";
+        parrafo.textContent = "Ingresa el Texto que deseas encriptar o desencriptar";
         alert("Ingresa el texto que deseas encriptar o desencriptar");
     }
 }
