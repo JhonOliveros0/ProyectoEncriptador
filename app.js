8fd3754662c3038fae2e4172270d42dcf5f7577f
JhonOliveros0 <jhonoliveros0@gmail.com>
2024-08-25 02:25:59 -0500

    agrega la funcion de desencriptar

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Funcion para encriptar el texto ingresado
 function encriptar() {
     let texto = document.getElementById("texto").value;
     let tituloMensaje = document.getElementById("titulo");
@@ -9,7 +10,7 @@ function encriptar() {
         .replace(/a/gi, "ai")
         .replace(/o/gi, "ober")
         .replace(/u/gi, "ufat");
-
+    // Condicional que valida que el texto no este vacio o no tenga caracteres.
     if (texto.length != 0) {
         document.getElementById("texto").value = textoEncriptado;
         tituloMensaje.textContent = "Texto Encriptado con éxito";
@@ -18,6 +19,32 @@ function encriptar() {
     } else {
         imagen.src = "./img/muñeco.png";
         tituloMensaje.textContent = "Ningún mensaje encontrado";
+        parrafo.textContent = "Ingresa el Texto que deseas encriptar o desencriptar";
+        alert("Ingresa el texto que deseas encriptar o desencriptar");
+    }
+}
+
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let titulo = document.getElementById("titulo");
+    let parrafo = document.getElementById("parrafo");
+    let imagen = document.getElementById("inicio");
+
+    let textoDesencriptado = texto
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
+
+    if (texto.length != 0) {
+        document.getElementById("texto").value = textoDesencriptado;
+        titulo.textContent = "Texto Desencriptado";
+        parrafo.textContent = "";
+        imagen.src = "img/Desencriptado.jpg";
+    } else {
+        imagen.src = "./img/muñeco.png";
+        titulo.textContent = "Ningún mensaje encontrado";
         parrafo.textContent = "Ingresa el Texto que deseas encriptar o desencriptar";
         alert("Ingresa el texto que deseas encriptar o desencriptar");
     }
